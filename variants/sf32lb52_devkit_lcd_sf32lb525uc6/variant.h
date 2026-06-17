cd4ebcf34bce744a13cc5efb0df803e91a7d1f43
abcdaaa1111 <859361238@qq.com>
2026-06-17 11:39:08 +0800

    feat(sf32lb52): add variant for sf32lb52_devkit_lcd board.

---
+++ variants/sf32lb52_devkit_lcd_sf32lb525uc6/variant.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define MOSI 14
+#define MISO 15
+#define SCK 18
+#define SS 19
+#define SDA 22
+#define SCL 20
+#define LED_BUILTIN 16
