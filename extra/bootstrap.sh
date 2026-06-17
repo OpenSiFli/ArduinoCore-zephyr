e4b2906d13652dfb18541756bd97e7789936ddc6
HalfSweet <HalfSweet@HalfSweet.cn>
2026-06-17 15:49:25 +0800

    fix(ci): rely on Zephyr Python requirements

--- extra/bootstrap.sh
+++ extra/bootstrap.sh
@@ -28,7 +28,7 @@ done
 log_msg "group" "Bootstrapping Python environment for Zephyr"
 python3 -m venv venv
 source venv/bin/activate
-pip install west protobuf grpcio-tools pyelftools
+pip install west protobuf grpcio-tools
 log_msg "endgroup"
 
 log_msg "group" "Initializing Zephyr workspace and modules: $HAL_FILTER"
